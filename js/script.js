540bfb31b473221b588050772aaa384e40c5872c
lyno4ka <yuliyamasterenko@gmail.com>
2020-03-12 01:00:19 +0200

    X-Editable

--- js/script.js
+++ js/script.js
@@ -1,37 +1,67 @@
+//ET - 10 hours, AT - 13 hours
+
 document.querySelector('body').addEventListener('click', clickHandler);
 
 function clickHandler(event) {
     if(!event.target.hasAttribute('editable')) return;
 
     event.preventDefault();
-    const { target: targetElement } = event;
+    const targetElement = event.target;
     const type = targetElement.getAttribute('editable');
-
-    console.log('type', type);
 
     const inputElement = document.createElement('input');
     inputElement.setAttribute('type', type);
+
     targetElement.parentNode.appendChild(inputElement);
     inputElement.value = targetElement.innerText;
+    inputElement.classList.add('form-control');
 
-    // targetElement.parentNode.removeChild(targetElement);
-    targetElement.remove();
+    let btnSave = document.createElement('button');
+    btnSave.classList.add('btn', 'btn-primary', 'btn-xs');
+    let iconBtnSave = document.createElement('span');
+    btnSave.appendChild(iconBtnSave);
+    btnSave.firstElementChild.classList.add('glyphicon', 'glyphicon-ok');
+    targetElement.parentNode.appendChild(btnSave);
+
+    let btnCancel = document.createElement('button');
+    btnCancel.classList.add('btn', 'btn-danger', 'btn-xs');
+    let iconBtnCancel = document.createElement('span');
+    btnCancel.appendChild(iconBtnCancel);
+    btnCancel.firstElementChild.classList.add('glyphicon', 'glyphicon-remove');
+    targetElement.parentNode.appendChild(btnCancel);
 
     // inputElement.focus();
     inputElement.select();
 
+    inputElement.addEventListener('blur', function(event) {
+        console.log(targetElement);
+        targetElement.parentNode.appendChild(targetElement);
+        console.log(  targetElement.parentNode.appendChild(targetElement));
+		targetElement.parentNode.removeChild(inputElement);
+		targetElement.parentNode.removeChild(btnSave);
+		targetElement.parentNode.removeChild(btnCancel);
+	});
+
+
     inputElement.addEventListener('keyup', function(event) {
         switch (event.which) {
             case 13:  //save
-                inputElement.parentNode.appendChild(targetElement);
                 targetElement.innerText = inputElement.value;
-                // targetElement.innerText = this.value;
-                inputElement.remove();
+                inputElement.blur();
                 break;
             case 27:  //cancel
-                inputElement.parentNode.appendChild(targetElement);
-                inputElement.remove();
+                inputElement.blur();
                 break;
-        }
+        }    
+    });
+
+    
+    btnSave.addEventListener('mousedown', function () {
+        targetElement.innerText = inputElement.value;
+        inputElement.blur();
+    });
+
+    btnCancel.addEventListener('mousedown', function() {
+        inputElement.blur();
     });
 }
